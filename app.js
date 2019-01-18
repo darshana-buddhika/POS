746e6187626ae71fbd2d41af2e2cb1d90550f09d
Darshana Buddhika <darshana.buddhika@syscolabs.com>
2019-01-18 12:55:02 +0530

    Db Added

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 const express = require('express');
 const app = express()
 
+const { secret, connectionUrl } = require('./config');
+const mongoose = require('mongoose');
+
 const auth = require('./auth/requiredAuthentication');
 
-app.use(express.json());
+// MongoDB connection 
+mongoose.connect(connectionUrl);
 
+mongoose.Promise = global.Promise;
+const db = mongoose.connection;
+
+// Database error handling
+db.on('error',  (e) => {
+    console.log(`Database connection error : ${e}`);
+});
+
+
+// Parse requrest body to JSON
+app.use(express.json());
 
 app.use('/api', auth);
 
-app.get('/api/name',(req,res) => {
-    res.send('Hello From The API..');
-}); 
+
 module.exports = app
